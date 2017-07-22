a94c6f51cbbc3720822e18dd5681472e1ae014a1
Slawek Zachcial <slawek.zachcial@gmail.com>
2017-07-22 21:30:08 +0200

    Replace CoffeeScript with JavaScript

---
+++ src/onsupportduty.js
@@ -0,0 +1,108 @@
+'use strict'
+// Description
+//   A hubot script that manages on-support-duty shifts and alerts
+//
+// Configuration:
+//   HUBOT_ON_SUPPORT_DUTY_SHIFTS="shift1=00:00-06:00,shift2=06:00-12:00,..."
+//
+// Commands:
+//   hubot shifts - prints shifts in UTC along assigned users
+//   hubot onsupportduty <shift> @user1 @user3 - assign users to shift
+//   hubot alerts - prints current alerts with assignees
+//   hubot alert ack <id> - accept working on alert
+//   hubot alert done <id> - confirm alerted problem fixed
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Slawek Zachcial
+
+module.exports = (robot) => {
+
+  // initialize it as empty so we don't have to check each time it was initialized
+  // TODO: use 'on loaded' event to initialize it
+  robot.brain.set("shifts", {})
+
+
+  class Shift {
+    constructor(name, start, end, users=[]) {
+      this.name = name
+      this.start = start
+      this.end = end
+      this.users = users
+    }
+
+    toString() {
+      return `${this.name}: ${this.start}-${this.end} UTC => ${this.users.length == 0 ? '[]' : this.users}`
+    }
+
+    remember() {
+      robot.brain.get("shifts")[this.name] = this
+    }
+
+    static fromString(shiftString) {
+      const matches = shiftString.match(/(.*)=(\d\d:\d\d)-(\d\d:\d\d)/)
+      if (!matches || matches.length != 4) {
+        throw new Error(`Shift format error: expected name=ss:ss-ee:ee but got: ${shiftString}`)
+      }
+      const [entireMatch, name, start, end] = matches
+      return new Shift(name, start, end)
+    }
+
+    static find(name) {
+      return robot.brain.get("shifts")[name]
+    }
+
+    static all() {
+      const shiftsInBrain = robot.brain.get("shifts")
+      return Object.entries(shiftsInBrain)
+        .map(([name, shift]) => shift);
+    }
+
+    static parse(shiftsString) {
+      return shiftsString.split(",").map(s => Shift.fromString(s))
+    }
+  }
+
+  // Give access to Shift class in tests
+  if (robot.adapterName.match(/mock-adapter/gi)) {
+    robot.Shift = Shift
+  }
+
+  (process.env.HUBOT_ON_SUPPORT_DUTY_SHIFTS
+    ? Shift.parse(process.env.HUBOT_ON_SUPPORT_DUTY_SHIFTS)
+    : [ new Shift("APJ", "00:00", "08:00"),
+        new Shift("EMEA", "08:00", "16:00"),
+        new Shift("AMS", "16:00", "00:00") ])
+    .forEach(shift => shift.remember())
+
+
+  robot.respond(/shifts/, (res) => {
+    const markdownList = Shift.all().map(s => `* ${s}`).join("\n")
+    res.reply(`Here are currently defined shifts:\n${markdownList}`)
+  })
+
+
+  robot.respond(/onsupportduty (.*)/, (res) => {
+    const words = res.match[1].split(/[\s,]+/)
+
+    if (words.length < 2) {
+      res.reply("Hmmm, you need to tell me the shift name and user(s)")
+      return
+    }
+
+    const shiftName = words[0]
+    const users = words.slice(1)
+
+    const existingShift = Shift.find(shiftName)
+    if (!existingShift) {
+      res.reply(`Hmmm, it seems ${shiftName} shift has not been defined`)
+      return
+    }
+
+    existingShift.users = users
+
+    res.reply(`Shift users recorded: ${existingShift}`)
+  })
+}
---
+++ test/onsupportduty-test.js
@@ -0,0 +1,117 @@
+'use strict'
+
+/* global describe, beforeEach, afterEach, it */
+
+const path = require('path')
+
+const chai = require('chai')
+const Hubot = require('hubot')
+
+const expect = chai.expect
+const Robot = Hubot.Robot
+const TextMessage = Hubot.TextMessage
+
+chai.use(require('sinon-chai'))
+
+describe('require("onsupportduty")', () => {
+  it('exports a function', () => {
+    expect(require('../index')).to.be.a('Function')
+  })
+})
+
+describe('onsupportduty', () => {
+  let robot, user
+
+  beforeEach(() => {
+    robot = new Robot(null, 'mock-adapter-v3', false, 'hubot')
+    robot.loadFile(path.resolve('src/'), 'onsupportduty.js')
+    robot.adapter.on('connected', () => {
+      robot.brain.userForId('1', {
+        name: 'alice',
+        real_name: 'Alice A',
+        room: '#test'
+      })
+    })
+    robot.run()
+    user = robot.brain.userForName('alice')
+  })
+
+  afterEach(() => {
+    robot.shutdown()
+  })
+
+
+  it('responds to shifts', (done) => {
+    robot.adapter.on('reply', function(envelope, strings) {
+      const answer = strings[0]
+      expect(answer).to.include('APJ').and.to.include('AMS').and.to.include('EMEA')
+      done()
+    })
+
+    robot.adapter.receive(new TextMessage(user, 'hubot shifts'))
+  })
+
+
+  it('responds to onsupportduty EMEA @bob @carol', (done) => {
+    robot.adapter.on('reply', function(envelope, strings) {
+      const answer = strings[0]
+      expect(answer).to.include('Shift users recorded')
+      done()
+    })
+
+    robot.adapter.receive(new TextMessage(user, '@hubot onsupportduty EMEA @bob @carol'))
+  })
+
+
+  it('responds to onsupportduty EMEA when users missing', (done) => {
+    robot.adapter.on('reply', function(envelope, strings) {
+      expect(strings[0]).to.include('Hmmm')
+      done()
+    })
+
+    robot.adapter.receive(new TextMessage(user, '@hubot onsupportduty EMEA'))
+  })
+
+
+  it('responds to onsupportduty XXX @bob when shift undefined', (done) => {
+    robot.adapter.on('reply', function(envelope, strings) {
+      expect(strings[0]).to.include("not").and.to.include("defined")
+      done()
+    })
+    robot.adapter.receive(new TextMessage(user, '@hubot onsupportduty XXX @bob'))
+  })
+
+
+  it('responds to shifts when users assigned', (done) => {
+    robot.adapter.on('reply', function(envelope, strings) {
+      const answer = strings[0]
+      if (answer.match(/recorded/gi)) {
+        // TODO: is there a better way to chain messages sent to hubot?
+        robot.adapter.receive(new TextMessage(user, 'hubot shifts'))
+      } else {
+        expect(answer).to.include('APJ').and.to.include('AMS').and.to.include('EMEA').and.to.include('@carol')
+        done()
+      }
+    })
+
+    robot.adapter.receive(new TextMessage(user, 'hubot onsupportduty APJ @alice @carol'))
+  })
+
+
+  it('can parse shifts string', (done) => {
+    const shifts = robot.Shift.parse("XX=00:00-01:00,YY=01:00-03:00,ZZ=03:00-00:00")
+    expect(shifts).to.have.lengthOf(3)
+    expect(shifts[1].name).to.equal("YY")
+    expect(shifts[2].start).to.equal("03:00")
+    expect(shifts[0].end).to.equal("01:00")
+    done()
+  })
+
+
+  it('detects error in shifts string', (done) => {
+    const fromString = () => robot.Shift.fromString("XX=:00-01")
+    expect(fromString).to.throw(/format/gi)
+    done()
+  })
+
+})
