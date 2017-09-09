12cbcdb6dbe8e9edb0a0682a70d15b2a80a5ecd0
Slawek Zachcial <slawek.zachcial@gmail.com>
2017-09-09 20:47:54 +0200

    Add Standard JS and fix resulting errors

--- src/alerts.js
+++ src/alerts.js
@@ -17,51 +17,49 @@
 //
 // Author:
 //   Slawek Zachcial
-
 module.exports = (robot) => {
-
   // initialize it as empty so we don't have to check each time it was initialized
   // TODO: use 'on loaded' event to initialize it
-  robot.brain.set("shifts", {})
-
+  robot.brain.set('shifts', {})
 
   class Shift {
-    constructor(name, start, end, users=[]) {
+    constructor (name, start, end, users = []) {
       this.name = name
       this.start = start
       this.end = end
       this.users = users
     }
 
-    toString() {
-      return `${this.name}: ${this.start}-${this.end} UTC => ${this.users.length == 0 ? '[]' : this.users}`
+    toString () {
+      return `${this.name}: ${this.start}-${this.end} UTC => ${this.users.length === 0 ? '[]' : this.users}`
     }
 
-    remember() {
-      robot.brain.get("shifts")[this.name] = this
+    remember () {
+      robot.brain.get('shifts')[this.name] = this
     }
 
-    static fromString(shiftString) {
+    static fromString (shiftString) {
       const matches = shiftString.match(/(.*)=(\d\d:\d\d)-(\d\d:\d\d)/)
-      if (!matches || matches.length != 4) {
+      if (!matches || matches.length !== 4) {
         throw new Error(`Shift format error: expected name=ss:ss-ee:ee but got: ${shiftString}`)
       }
-      const [entireMatch, name, start, end] = matches
+      // ignore first element of the array returned by "matches"
+      const [, name, start, end] = matches
       return new Shift(name, start, end)
     }
 
-    static find(name) {
-      return robot.brain.get("shifts")[name]
+    static find (name) {
+      return robot.brain.get('shifts')[name]
     }
 
-    static all() {
-      const shiftsInBrain = robot.brain.get("shifts")
+    static all () {
+      const shiftsInBrain = robot.brain.get('shifts')
       return Object.entries(shiftsInBrain)
-        .map(([name, shift]) => shift);
+        .map(([name, shift]) => shift)
     }
 
-    static parse(shiftsString) {
-      return shiftsString.split(",").map(s => Shift.fromString(s))
+    static parse (shiftsString) {
+      return shiftsString.split(',').map(s => Shift.fromString(s))
     }
   }
 
@@ -72,23 +70,19 @@ module.exports = (robot) => {
 
   (process.env.HUBOT_SHIFTS
     ? Shift.parse(process.env.HUBOT_SHIFTS)
-    : [ new Shift("APJ", "00:00", "08:00"),
-        new Shift("EMEA", "08:00", "16:00"),
-        new Shift("AMS", "16:00", "00:00") ])
+    : [ new Shift('APJ', '00:00', '08:00'), new Shift('EMEA', '08:00', '16:00'), new Shift('AMS', '16:00', '00:00') ])
     .forEach(shift => shift.remember())
 
-
   robot.respond(/shifts$/, (res) => {
-    const markdownList = Shift.all().map(s => `* ${s}`).join("\n")
+    const markdownList = Shift.all().map(s => `* ${s}`).join('\n')
     res.reply(`Here are currently defined shifts:\n${markdownList}`)
   })
-
 
   robot.respond(/shifts (.*)/, (res) => {
     const words = res.match[1].split(/[\s,]+/)
 
     if (words.length < 2) {
-      res.reply("Hmmm, you need to tell me the shift name and user(s)")
+      res.reply('Hmmm, you need to tell me the shift name and user(s)')
       return
     }
 
--- test/alerts-test.js
+++ test/alerts-test.js
@@ -40,9 +40,8 @@ describe('alerts', () => {
     robot.shutdown()
   })
 
-
   it('responds to shifts', (done) => {
-    robot.adapter.on('reply', function(envelope, strings) {
+    robot.adapter.on('reply', function (envelope, strings) {
       const answer = strings[0]
       expect(answer).to.include('APJ').and.to.include('AMS').and.to.include('EMEA')
       done()
@@ -51,9 +50,8 @@ describe('alerts', () => {
     robot.adapter.receive(new TextMessage(user, 'hubot shifts'))
   })
 
-
   it('responds to shifts EMEA @bob @carol', (done) => {
-    robot.adapter.on('reply', function(envelope, strings) {
+    robot.adapter.on('reply', function (envelope, strings) {
       const answer = strings[0]
       expect(answer).to.include('Shift users recorded')
       done()
@@ -62,9 +60,8 @@ describe('alerts', () => {
     robot.adapter.receive(new TextMessage(user, '@hubot shifts EMEA @bob @carol'))
   })
 
-
   it('responds to shifts EMEA when users missing', (done) => {
-    robot.adapter.on('reply', function(envelope, strings) {
+    robot.adapter.on('reply', function (envelope, strings) {
       expect(strings[0]).to.include('Hmmm')
       done()
     })
@@ -72,18 +69,16 @@ describe('alerts', () => {
     robot.adapter.receive(new TextMessage(user, '@hubot shifts EMEA'))
   })
 
-
   it('responds to shifts XXX @bob when shift undefined', (done) => {
-    robot.adapter.on('reply', function(envelope, strings) {
-      expect(strings[0]).to.include("not").and.to.include("defined")
+    robot.adapter.on('reply', function (envelope, strings) {
+      expect(strings[0]).to.include('not').and.to.include('defined')
       done()
     })
     robot.adapter.receive(new TextMessage(user, '@hubot shifts XXX @bob'))
   })
 
-
   it('responds to shifts when users assigned', (done) => {
-    robot.adapter.on('reply', function(envelope, strings) {
+    robot.adapter.on('reply', function (envelope, strings) {
       const answer = strings[0]
       if (answer.match(/recorded/gi)) {
         // TODO: is there a better way to chain messages sent to hubot?
@@ -97,21 +92,18 @@ describe('alerts', () => {
     robot.adapter.receive(new TextMessage(user, 'hubot shifts APJ @alice @carol'))
   })
 
-
   it('can parse shifts string', (done) => {
-    const shifts = robot.Shift.parse("XX=00:00-01:00,YY=01:00-03:00,ZZ=03:00-00:00")
+    const shifts = robot.Shift.parse('XX=00:00-01:00,YY=01:00-03:00,ZZ=03:00-00:00')
     expect(shifts).to.have.lengthOf(3)
-    expect(shifts[1].name).to.equal("YY")
-    expect(shifts[2].start).to.equal("03:00")
-    expect(shifts[0].end).to.equal("01:00")
+    expect(shifts[1].name).to.equal('YY')
+    expect(shifts[2].start).to.equal('03:00')
+    expect(shifts[0].end).to.equal('01:00')
     done()
   })
 
-
   it('detects error in shifts string', (done) => {
-    const fromString = () => robot.Shift.fromString("XX=:00-01")
+    const fromString = () => robot.Shift.fromString('XX=:00-01')
     expect(fromString).to.throw(/format/gi)
     done()
   })
-
 })
